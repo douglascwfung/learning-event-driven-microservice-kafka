120ae366dd211b1a12a72603940ae78c0f199c46
Douglas Fung <icestone.net@gmail.com>
2022-10-22 12:25:27 +0800

    completed section 4

---
+++ docker-compose/check-config-server-started.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# check-config-server-started.sh
+
+apt-get update -y
+
+yes | apt-get install curl
+
+curlResult=$(curl -s -o /dev/null -I -w "%{http_code}" http://config-server:8888/actuator/health)
+
+echo "result status code:" "$curlResult"
+
+while [[ ! $curlResult == "200" ]]; do
+  >&2 echo "Config server is not up yet!"
+  sleep 2
+  curlResult=$(curl -s -o /dev/null -I -w "%{http_code}" http://config-server:8888/actuator/health)
+done
+
+echo "****************************************************"
+echo "Config server is ready!"
+echo "****************************************************"
+
+./cnb/lifecycle/launcher
