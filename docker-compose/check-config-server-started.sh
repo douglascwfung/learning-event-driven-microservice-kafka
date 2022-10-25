1104d7e7a3629dcc3f6f675a801dfb20dc8955aa
Douglas Fung <icestone.net@gmail.com>
2022-10-25 15:31:33 +0800

    complete section 6 - 55

--- docker-compose/check-config-server-started.sh
+++ docker-compose/check-config-server-started.sh
@@ -15,8 +15,4 @@ while [[ ! $curlResult == "200" ]]; do
   curlResult=$(curl -s -o /dev/null -I -w "%{http_code}" http://config-server:8888/actuator/health)
 done
 
-echo "****************************************************"
-echo "Config server is ready!"
-echo "****************************************************"
-
 ./cnb/lifecycle/launcher
